71e763807f07bf11fd2df846c3c5d19d05459efe
nobu0205 <nobuhiro.dream@gmail.com>
2026-04-08 14:52:12 +0900

    fix: disable turbo for dynamic form

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,11 @@
+// app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
+// Stimulus アプリケーションを起動
 const application = Application.start()
+
+// controllers フォルダから全コントローラーを自動読み込み
+eagerLoadControllersFrom("controllers", application)
 
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
-# config/importmap.rb
-
-pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# Stimulus controllers
+pin "application", to: "controllers/application.js"      
 pin_all_from "app/javascript/controllers", under: "controllers"
