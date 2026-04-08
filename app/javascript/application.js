6985de861bd1584bdad57c65ecb26c4615feed33
nobu0205 <nobuhiro.dream@gmail.com>
2026-04-08 14:14:34 +0900

    fix: stimulus loading

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,2 @@
-// Entrypoint
 import "@hotwired/turbo-rails"
-import { Application } from "@hotwired/stimulus"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-
-// Stimulus application を作成
-const application = Application.start()
-
-// controllers フォルダから自動でコントローラーを読み込む
-eagerLoadControllersFrom("controllers", application)
-
-export { application }
+import "controllers"
